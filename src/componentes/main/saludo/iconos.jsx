16ce624011eb149d8633933092062bd1e4e5a1cd
DavidValdezateGarciaLlefia <120660026+DavidValdezateGarciaLlefia@users.noreply.github.com>
2024-05-10 16:56:36 +0200

    Delete src directory

--- src/componentes/main/saludo/iconos.jsx
+++
@@ -1,7 +0,0 @@
-export function Icono({icono}){
-    return(
-       <div className="items-center p-2 w-10 h-10 justify-center">
-           <img className="" src={icono} alt="icono"></img>
-       </div> 
-    )
-}
--- src/componentes/main/sobre/habilidades.jsx
+++
@@ -1,7 +0,0 @@
-export function Habilidades({ habilidad }) {
-  return (
-    <div className="bg-gray-200 text-gray-800 py-2 px-4 rounded shadow m-2 text-center cursor-default">
-      {habilidad}
-    </div>
-  );
-}
